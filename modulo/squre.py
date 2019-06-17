ce8416ab34bead438c39f2e0c3ff9de44637926f
sujatha <kmaheshwar@localhost.localdomain>
2019-06-17 15:54:20 +0530

    YYYYYYYYYYYYYYYY added modified squre.py file

--- modulo/squre.py
+++ modulo/squre.py
@@ -1,5 +1,5 @@
-mod =19
-print(" mod = ",mod)
+mod =11
 for x in range(1,mod):
-  print(x ,"*", x  ," = ", (x*x)%mod)
-  #print((x**2)%11)
+ # print(x ,"*",,"*", x,"%",mod," = ", (x**x)%mod)
+  print(x,"^",2,"%",mod,"=",pow(x,2,mod)) 
+      #print((x**2)%11)
